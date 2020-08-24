666039db64daa8d97dd1e5a32fd05c1ab7de36c2
Igor Augusto Dos Santos <igoraugusto1029@gmail.com>
2020-08-23 21:00:39 -0300

    Definição da estrutura

---
+++ Listas lineares/Lista Sequencial.cpp
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+#define MAX 50
+
+typedef struct{
+    int A[MAX];
+    int nroElementos;
+}LISTA;
+
+typedef struct{
+    int Id;
+}REGISTRO;
+
+void inicializarLista(LISTA *l){
+    l->nroElementos=0;
+}
+
+int main(){
+
+}
+
