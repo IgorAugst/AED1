67aedcd094bfbbc5d13c1d61b082917b6db7f1e9
IgorSantoss <igoraugusto1029@gmail.com>
2020-08-24 16:45:15 -0300

    Operações lista sequencial

--- Listas lineares/Lista Sequencial.cpp
+++ Listas lineares/Lista Sequencial.cpp
@@ -4,13 +4,13 @@
 #define MAX 50
 
 typedef struct{
+    int Id;
+}REGISTRO;
+
+typedef struct{
     REGISTRO A[MAX];
     int nroElementos;
 }LISTA;
-
-typedef struct{
-    int Id;
-}REGISTRO;
 
 void inicializarLista(LISTA *l){
     l->nroElementos=0;
@@ -22,18 +22,66 @@ int tamanho(LISTA *l){
 
 void exibirElementos(LISTA *l){
     for(int i=0; i<l->nroElementos; i++){
-        printf("%d", l->A[i].Id);
+        printf("%d\n", l->A[i].Id);
     }
 }
 
 int buscaSequencial(LISTA *l, int Id){
     for(int i=0; i<l->nroElementos; i++){
-        if(l->A[i].id == Id) return i;
+        if(l->A[i].Id == Id) return i;
     }
     return -1;
 }
 
+bool inserirElemento(LISTA *l, int pos, REGISTRO r){
+    if(pos>=0 && pos<=l->nroElementos && l->nroElementos!=MAX){
+        for(int i=l->nroElementos; i>pos; i--){
+            l->A[i]=l->A[i-1];
+        }
+
+        l->A[pos]=r;
+        l->nroElementos++;
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool excluirElemento(LISTA *l, int Id){
+    int pos = buscaSequencial(l,Id);
+
+    if(pos==-1) return false;
+
+    for(int i = pos; i<l->nroElementos-1; i++){
+        l->A[i] = l->A[i+1];
+    }
+
+    l->nroElementos--;
+    return true;
+}
+
+void reiniciarLista(LISTA *l){
+    l->nroElementos=0;
+}
+
 int main(){
 
+    LISTA l;
+    inicializarLista(&l);
+
+    for(int i=0; i<10; i++){
+        REGISTRO a;
+        a.Id=i;
+        inserirElemento(&l,i,a);
+    }
+
+    REGISTRO a;
+    a.Id=95;
+    inserirElemento(&l,5,a);
+
+    exibirElementos(&l);
+    excluirElemento(&l,6);
+    printf("\n\n");
+    exibirElementos(&l); 
 }
 
