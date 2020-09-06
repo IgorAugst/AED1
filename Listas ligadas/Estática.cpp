a67234bbd2b40ffe0afcb6d0f473f1c0b87678c6
IgorSantoss <igoraugusto1029@gmail.com>
2020-09-06 15:43:53 -0300

    finalização lista ligada estática

--- Listas ligadas/Estática.cpp
+++ Listas ligadas/Estática.cpp
@@ -32,7 +32,7 @@ void exibirLista(LISTA *l){
     int pos=l->inicio;
 
     while(pos != -1){
-        printf("%d", l->A[pos].Id);
+        printf("%d\n", l->A[pos].Id);
         pos=l->A[pos].proximo;
     }
 }
@@ -52,6 +52,61 @@ int buscaSequencial(LISTA *l, int chave, int* ant){
     else return -1;
 }
 
-int main(){
+int obterNoDisp(LISTA *l){
+    int result = l->dispo;
+    if(result > -1){
+        l->dispo=l->A[result].proximo;
+    }
+    return result;
+}
 
+void devolverNoDisp(LISTA *l, int pos){
+    l->A[pos].proximo = l->dispo;
+    l->dispo = pos;
+}
+
+bool excluirElemento(LISTA *l, int chave){
+    int ant;
+    int pos = buscaSequencial(l, chave, &ant);
+
+    if(pos<0) return false;
+    if(ant == -1) l->inicio = l->A[pos].proximo;
+    else l->A[ant].proximo = l->A[pos].proximo;
+    devolverNoDisp(l, pos);
+    return true;
+}
+
+bool inserirElementoOrd(LISTA *l, int chave){
+    int ant;
+    int pos = buscaSequencial(l, chave, &ant);
+
+    if(l->dispo < 0 || pos != -1) return false; //retorna falso caso não tenha espaço disponível ou caso o elemento já exista
+    pos = obterNoDisp(l);
+    l->A[pos].Id = chave;
+
+    if(l->inicio < 0){ //se o inicio for menor do que 0 significa que é o primeiro elemento
+        l->inicio = pos;
+        l->A[pos].proximo = -1;
+    }else{
+        if(ant < 0){ //primeiro elemento
+            l->A[pos].proximo = l->inicio;
+            l->inicio = pos;
+        }else{ //entre elementos
+            l->A[pos].proximo = l->A[ant].proximo;
+            l->A[ant].proximo = pos;
+        }
+    }
+
+    return true;
+}
+
+int main(){
+    LISTA teste;
+    inicializar(&teste);
+    
+    for(int i=0; i<10; i++){
+        inserirElementoOrd(&teste, i);
+    }
+    
+    exibirLista(&teste);
 }
